49a5d8ddf94f63696d5660b2df5efeee4d70ad25
Mahay <pengfeizhao316@gmail.com>
2020-02-29 16:26:56 +0800

    2月21日Python练习

---
+++ Exercise/0221/header.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python3
+# -*- encoding: utf-8 -*-
+'''
+@File : header.py
+@Time : 2020/02/26 00:59:16
+@Author : Mahay 
+@Version : 1.0
+'''
+
+# 尝试使用User Snippet生成header
+# 输出简单字符串
+str = '我爱中国!'
+print(str)
