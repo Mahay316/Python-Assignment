e562f381b6ce4165bb2a4ead75e36780892aaffd
Mahay <pengfeizhao316@gmail.com>
2020-06-30 19:16:48 +0800

    删除了无用文件

--- Homework/Homework11/bbs-demo/convert.py
+++
@@ -1,4 +0,0 @@
-with open('a.html', 'r', encoding='utf-8') as src:
-    with open('b.js', 'w', encoding='utf-8') as dst:
-        for line in src:
-            dst.write(f'content += \'{line.strip()}\';\n')
